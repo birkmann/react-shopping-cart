cea011784945369444a9a1f61ae73a248462230a
Rafael Birkmann <mail@rafaelbirkmann.de>
2020-03-25 12:55:08 +0100

    files

--- src/components/product/index.jsx
+++ src/components/product/index.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import "./index.scss";
 
+const addFunc = props => {
+  console.log("clicked");
+};
+
 export const Product = props => {
   return (
-    <div className="product">
+    <div className='product'>
       <img src={props.image} alt={props.name} />
-      <div className="details">
-        <div className="brand">{props.brand}</div>
-        <div className="name">{props.name}</div>
-        <div className="price">{props.price} $</div>
+      <div className='details'>
+        <div className='brand'>{props.brand}</div>
+        <div className='name'>{props.name}</div>
+        <div className='price'>{props.price} $</div>
       </div>
-      <button>Add to cart</button>
+      <button onClick={() => addFunc({ props })}>Add to cart</button>
     </div>
   );
 };
